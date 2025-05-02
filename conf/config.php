adf8891fde0dfa4e4903e335885d2f0cb867e47d
nabildzr <muhammadnbillz@gmail.com>
2025-05-02 17:56:03 +0700

    make it better

--- conf/config.php
+++ conf/config.php
@@ -1,9 +1,14 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'db_restaurant';
+// $host = 'localhost';
+// $user = 'root';
+// $password = '';
+// $database = 'db_restaurant';
 
-?>
+  
 
-<!--  -->
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$user = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
