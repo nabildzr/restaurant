469d212952dd215f915bbe2b8fd98fd0d7291fc7
nabildzr <muhammadnbillz@gmail.com>
2025-05-02 19:25:16 +0700

    fix: update require path in connection file to use relative reference for improved portability

--- conf/connection.php
+++ conf/connection.php
@@ -14,7 +14,7 @@
 
 
 // Load database credentials from a secure location
-require_once __DIR__ .  '/conf/config.php';
+require_once __DIR__ .  '/config.php';
 
 // Create a connection using mysqli
 $conn = new mysqli($host, $user, $password, $database);
